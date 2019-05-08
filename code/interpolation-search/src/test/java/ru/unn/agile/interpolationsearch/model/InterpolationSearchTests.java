cbe987781bb5573f6a037cdabb930250bfc18a65
unknown <nickprovidohin@gmail.com>
2019-05-09 01:52:32 +0300

    Check FixStyle ver4

--- code/interpolation-search/src/test/java/ru/unn/agile/interpolationsearch/model/InterpolationSearchTests.java
+++ code/interpolation-search/src/test/java/ru/unn/agile/interpolationsearch/model/InterpolationSearchTests.java
@@ -3,7 +3,6 @@ package ru.unn.agile.interpolationsearch.model;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class InterpolationSearchTests {
 
