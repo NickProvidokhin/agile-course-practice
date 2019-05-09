0665898f2deb297575d203090cf3e05d7a0a218d
unknown <nickprovidohin@gmail.com>
2019-05-09 03:16:53 +0300

    Check Fix checkstyle version final v 2

--- code/interpolation-search/src/test/java/ru/unn/agile/interpolationsearch/model/InterpolationSearchTests.java
+++ code/interpolation-search/src/test/java/ru/unn/agile/interpolationsearch/model/InterpolationSearchTests.java
@@ -28,7 +28,7 @@ public class InterpolationSearchTests {
 
     @Test
     public void searchKeyInMiddle() {
-        int[] array = {0, 1, 2, 3, 5, 6,7};
+        int[] array = {0, 1, 2, 3, 5, 6, 7};
         int key = 3;
         InterpolationSearch interpolationSearch = new InterpolationSearch(array);
         assertEquals(3, interpolationSearch.interpSearch(key));
@@ -36,7 +36,7 @@ public class InterpolationSearchTests {
 
     @Test
     public void searchKeyInLeft() {
-        int[] array = {0, 1, 2, 3, 5, 6,7};
+        int[] array = {0, 1, 2, 3, 5, 6, 7};
         int key = 1;
         InterpolationSearch interpolationSearch = new InterpolationSearch(array);
         assertEquals(1, interpolationSearch.interpSearch(key));
@@ -44,7 +44,7 @@ public class InterpolationSearchTests {
 
     @Test
     public void searchKeyRight() {
-        int[] array = {0, 1, 2, 3, 5, 6,7};
+        int[] array = {0, 1, 2, 3, 5, 6, 7};
         int key = 6;
         InterpolationSearch interpolationSearch = new InterpolationSearch(array);
         assertEquals(5, interpolationSearch.interpSearch(key));
@@ -52,7 +52,7 @@ public class InterpolationSearchTests {
 
     @Test
     public void searchIsNotKey() {
-        int[] array = {0, 1, 2, 3, 5, 6,7};
+        int[] array = {0, 1, 2, 3, 5, 6, 7};
         int key = 4;
         InterpolationSearch interpolationSearch = new InterpolationSearch(array);
         assertEquals(-1, interpolationSearch.interpSearch(key));
