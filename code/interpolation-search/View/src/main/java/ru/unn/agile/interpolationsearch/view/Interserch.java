9e797afac3e8a87aa49be6ec1ff9205e9b705f07
NickPro <nickprovidohin@gmail.com>
2019-05-13 03:30:29 +0300

    Connect ALL

--- code/interpolation-search/View/src/main/java/ru/unn/agile/interpolationsearch/view/Interserch.java
+++ code/interpolation-search/View/src/main/java/ru/unn/agile/interpolationsearch/view/Interserch.java
@@ -5,10 +5,16 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import ru.unn.agile.interpolationsearch.viewmodel.ViewModel;
+import ru.unn.agile.interpolationsearch.infrastructure.TxtLogger;
+
+
+import java.io.IOException;
 
 public class Interserch {
+    private static final String LOG_PATH = "log";
     @FXML
     private ViewModel viewModel;
     @FXML
@@ -18,14 +24,24 @@ public class Interserch {
     @FXML
     private Button button;
     @FXML
-    private Label labelStatus;
+    private Label labelStat;
     @FXML
-    private Label labelResult;
+    private Label labelRes;
+    @FXML
+    private TextArea txtArea;
     void initialize() {
+        try {
+            TxtLogger txtLogger = new TxtLogger(LOG_PATH);
+            viewModel.setLogger(txtLogger);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         arrInput.textProperty().bindBidirectional(viewModel.arrInputProperty());
         keyInput.textProperty().bindBidirectional(viewModel.keyInputProperty());
-        labelStatus.textProperty().bindBidirectional(viewModel.statusProperty());
-        labelResult.textProperty().bindBidirectional(viewModel.resultProperty());
+        labelStat.textProperty().bindBidirectional(viewModel.statusProperty());
+        labelRes.textProperty().bindBidirectional(viewModel.resultProperty());
+
+        txtArea.textProperty().bindBidirectional(viewModel.logProperty());
 
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
